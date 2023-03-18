b65c4cc79847bf6c746ce5d0a4a6cf84b3d88ef7
dbnor0 <dbnor0@gmail.com>
2023-03-18 14:49:02 +0200

    Cycle detection refactor

--- app/Analysis/Module.hs
+++ app/Analysis/Module.hs
@@ -16,6 +16,9 @@ import Data.Foldable
 import Analysis.Error
 import Data.Map as M
 
+
+type ImportGraph = M.Map Identifier [Identifier]
+
 checkCyclicImports :: BuildContext m =>  m ()
 checkCyclicImports = do
   ms <- gets (M.elems . (^. modules))
@@ -45,19 +48,18 @@ uniqueId :: BuildContext m
          -> m ()
 uniqueId id env err = when (id `elem` env) (throwError $ err id)
 
-detectCycles :: M.Map Identifier [Identifier] -> Bool
+detectCycles ::ImportGraph -> Bool
 detectCycles graph = and $ evalState (traverse go (M.keys graph)) (graph, [])
 
-go :: Identifier -> State (M.Map Identifier [Identifier], [Identifier]) Bool
+go :: Identifier -> State (ImportGraph, [Identifier]) Bool
 go id = do
   (graph, vis) <- get
   if duplicates vis then
     return True
   else
     case M.lookup id graph of
-      Nothing -> return False
-      Just [] -> return False
-      Just is -> do
+      Just is@(_:_) -> do
         modify $ second (id :)
         and <$> traverse go is
+      _ -> return False
   where duplicates = (/=) <$> (length . nubOrd) <*> length
